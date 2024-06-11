7afe14ffe5835d665dc30da7a14b1d5b50ec144f
Arun Sankar <imarunsankarks@gmail.com>
2024-06-11 10:39:31 +0530

    added detetion confirmation

--- src/components/ActivityDetails.js
+++ src/components/ActivityDetails.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const ActivityDetails = (props) => {
   const { activity, cardVariants, index, onUpdate, onDelete } = props;
@@ -15,12 +15,15 @@ const ActivityDetails = (props) => {
   }
 
   const deleteActivity = async (id) => {
-    const response = await fetch(`${process.env.REACT_APP_BE_URL}/api/routes/` + id, {
-      method: "DELETE",
-      headers: {
-        'Authorization': `Bearer ${user.token}`
-      },
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_BE_URL}/api/routes/` + id,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }
+    );
 
     if (!response.ok) {
       const json = await response.json();
@@ -34,48 +37,76 @@ const ActivityDetails = (props) => {
 
   const updateValue = async (id, date) => {
     const updatedActivity = { title, cost, date };
-    const response = await fetch(`${process.env.REACT_APP_BE_URL}/api/routes/` + id, {
-      method: "PATCH",
-      body: JSON.stringify(updatedActivity),
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${user.token}`
-      },
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_BE_URL}/api/routes/` + id,
+      {
+        method: "PATCH",
+        body: JSON.stringify(updatedActivity),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      }
+    );
 
     if (!response.ok) {
       const json = await response.json();
       toast.error(json.error);
     } else {
       const time = await response.json();
-      const createdAt = (time.createdAt);
+      const createdAt = time.createdAt;
       const updatedAt = new Date().toISOString();
-      onUpdate({ _id: id, title, cost, date, createdAt, updatedAt })
+      onUpdate({ _id: id, title, cost, date, createdAt, updatedAt });
       // onUpdate();
-      toast.success('Expense updated');
+      toast.success("Expense updated");
     }
   };
 
   const updatedAtDate = new Date(activity.updatedAt);
   const differenceInMs = Date.now() - updatedAtDate.getTime();
   const differenceInMinutes = Math.floor(differenceInMs / (1000 * 60));
-  const differenceInHr = differenceInMinutes > 60 ? Math.floor(differenceInMinutes / 60) : 0;
+  const differenceInHr =
+    differenceInMinutes > 60 ? Math.floor(differenceInMinutes / 60) : 0;
 
   const showUpdate = (index) => {
     const updateForm = document.querySelectorAll(".update");
     updateForm[index].classList.toggle("show");
   };
 
+  const showDelete = (index) => {
+    const deleteForm = document.querySelectorAll(".delete-confirm");
+    deleteForm[index].classList.toggle("show");
+  };
+
   return (
     <motion.div className="card each-activity" variants={cardVariants}>
       <div className="alter-buttons">
-        <button onClick={() => { showUpdate(index); }}>E</button>
-        <button onClick={() => { deleteActivity(activity._id); }}>x</button>
+        <button
+          onClick={() => {
+            showUpdate(index);
+          }}
+        >
+          E
+        </button>
+        <button
+          onClick={() => {
+            showDelete(index);
+          }}
+        >
+          x
+        </button>
       </div>
-      {activity.date && <span>Added on {activity.date.split('T')[0]}</span>}
+      {activity.date && <span>Added on {activity.date.split("T")[0]}</span>}
       <h3>{activity.title}</h3>
-      <p><span>Rs. </span>{activity.cost}</p>
-      {differenceInHr > 0 ? <span>{`Updated ${differenceInHr} hours ago`}</span> : <span>{`Updated ${differenceInMinutes} minutes ago`}</span>}
+      <p>
+        <span>Rs. </span>
+        {activity.cost}
+      </p>
+      {differenceInHr > 0 ? (
+        <span>{`Updated ${differenceInHr} hours ago`}</span>
+      ) : (
+        <span>{`Updated ${differenceInMinutes} minutes ago`}</span>
+      )}
 
       <form
         className="update"
@@ -87,19 +118,52 @@ const ActivityDetails = (props) => {
       >
         <input
           type="text"
-          onChange={(e) => { setTitle(e.target.value); }}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
           value={title}
         />
         <input
           type="text"
-          onChange={(e) => { setCost(e.target.value); }}
+          onChange={(e) => {
+            setCost(e.target.value);
+          }}
           value={cost}
         />
         <button>U</button>
-        <span className="updateFormClose" onClick={() => { showUpdate(index); }}>Close</span>
+        <span
+          className="updateFormClose"
+          onClick={() => {
+            showUpdate(index);
+          }}
+        >
+          C
+        </span>
         <Toaster />
       </form>
 
+      <div className="delete-confirm">
+        <h4>Delete?</h4>
+        <div className="buttons">
+        <button
+            className="yes"
+            onClick={() => {
+              deleteActivity(activity._id);
+            }}
+          >
+            Y
+          </button>
+          <button
+            className="no"
+            onClick={() => {
+              showDelete(index);
+            }}
+          >
+            N
+          </button>
+       
+        </div>
+      </div>
     </motion.div>
   );
 };
